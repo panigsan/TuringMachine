7df4c68908e560a0d5f0c9a0dbfbbc2c646403a5
panigsan <panigsan@students.zhaw.ch>
2016-11-16 15:54:25 +0100

    tm to data

--- src/TuringMachine.hs
+++ src/TuringMachine.hs
@@ -1,13 +1,40 @@
 module TuringMachine where
 
+
 type State      = Int
-type AlphaSymb  = Int       -- what I can read
-type BlankSymb  = AlphaSymb
-type InputSymb  = AlphaSymb -- what I can write
-data Direction  = Left | Right
-type PartFun    = State -> AlphaSymb -> (State, InputSymb, Direction)
+type Symbol     = Char
+data Direction  = L | R
+    deriving (Show)
+type PartFun    = State -> Symbol -> (State, Symbol, Direction)
 
+data Machine = Machine 
+               { states         :: [ State ]
+               , tapeAlphabet   :: [ Symbol ]
+               , blankSymbol    :: Symbol 
+               , inputSymbol    :: [ Symbol ]
+               , partFun        :: PartFun
+               , initialState   :: State
+               , finalStates    :: [ State]
+               }
+    --deriving (Show)
 
-f :: Int -> Int
-f a = a + 1
+-- Simple TM
+fun :: PartFun
+fun 0 '0' = (0, '1', R)
+fun 0 '1' = (0, '0', R)
+fun 0 ' ' = (1, ' ', R)
+
+tm  = Machine 
+     { states       = [0, 1]
+     , tapeAlphabet = ['0', '1', ' ']
+     , blankSymbol  = ' '
+     , inputSymbol  = ['0', '1']
+     , partFun      = fun
+     , initialState = 0
+     , finalStates  = [1]
+     }
+
+printFun :: PartFun -> String
+printFun (state symbol (a, b, c)) = "aa"
+
 
